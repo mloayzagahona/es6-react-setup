9e296b98b9975fc75d62268ea81ca2d411b73f54
Manuel Loayza <manuel.loayza@capitalone.com>
2016-10-18 03:03:11 -0400

    feat(egghead): Lab for Video 5
    https://egghead.io/lessons/react-state-basics

--- App.js
+++ App.js
@@ -1,8 +1,23 @@
 import React from 'react';
 
 export default class App extends React.Component{
-	render(){
-		return <h1>Hi Dylan & Angie</h1>
+  constructor(){
+    super();
+    this.state = {
+      txt: 'Hi Dylan & Angie',
+      cat: 6
+    };
+  }
+  update(e) {
+    this.setState({txt:e.target.value});
+  }
+	render() {
+		return(
+       <div>
+        <input type="text" onChange={this.update.bind(this)} />
+        <h1>{this.state.txt}</h1>
+      </div>
+    );
 	}
 }
 
--- main.js
+++ main.js
@@ -1,24 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-// import App from './App'
-
-class App extends React.Component {
-	render(){
-    let txt = this.props.txt;
-		return <h1>{txt}</h1>
-	}
-}
-
-App.propTypes = {
-  txt: React.PropTypes.string,
-  cat: React.PropTypes.number.isRequired
-}
-
-App.defaultProps = {
-  txt: 'I love my childre and my wife'
-}
+import App from './App'
 
 ReactDom.render(
-	<App cat={5}  />, 
+	<App  />, 
   document.getElementById('app')
 );
