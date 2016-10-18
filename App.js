14043f6b65ca9965d09f8252c4d27974ca657d1e
Manuel Loayza <manuel.loayza@capitalone.com>
2016-10-18 03:52:30 -0400

    feat(egghead): Lab for Video 8
    https://egghead.io/lessons/react-accessing-child-properties

--- App.js
+++ App.js
@@ -1,51 +1,16 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-export default class App extends React.Component{
-  constructor(){
-    super();
-    this.state = {
-      red: 0, 
-      green: 0,
-      blue:0
-    };
-    this.update = this.update.bind(this);
-  }
-  update(e) {
-    this.setState({
-      red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value, 
-      green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value, 
-      blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value, 
-    });
-  }
-	render() {
-		return(
-       <div>
-       {this.state.txt}
-       <hr/>
-        <Slider ref="red" update={this.update} />
-        {this.state.red}
-        <br />
-        <Slider ref="green" update={this.update} />
-        {this.state.green}
-        <br />
-        <Slider ref="blue" update={this.update} />
-        {this.state.blue}
-        <br />
-      </div>
-    );
+class Button extends React.Component {
+	render () {
+		return <button>I <Heart/> React </button>
 	}
 }
 
-class Slider extends React.Component{
+const Heart = () => <span className="glyphicon glyphicon-heart"></span>
+
+export default class App extends React.Component{
   render() {
-		return(
-        <div>
-        <input ref="inp" type="range"
-          min = "0" max="255"
-          onChange={this.props.update} />
-        </div>
-    );    
+  	return <Button>React</Button>;
   }
 }
 
